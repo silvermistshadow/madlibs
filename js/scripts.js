3c94b181f1dc7f161cc3891e4827c743e895908d
silvermistshadow <bmead72@gmail.com>
2019-02-08 11:14:47 -0800

    add javascript to handle the form

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    var person1Input = $("input#person1").val();
+    var verbInput = $("input#verb").val();
+    var actverbInput = $("input#actverb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".verb").text(verbInput);
+    $(".acterb").text(actverbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+
+  });
+
+});
